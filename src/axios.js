568376f4e215f524c6990e0401d11e7725692aed
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 22:43:32 -0300

    test proxy

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
-  headers: {
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    'Access-Control-Allow-Credentials': true,
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
+  baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/'
 });
 export default instance;
