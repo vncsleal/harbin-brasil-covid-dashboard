6ae3d19f26e4287810dd424c1a0072ef9a66b7d4
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 23:19:42 -0300

    headers

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
   headers: {
-    'Access-Control-Allow-Origin' : 'https://api.brasil.io',
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
+  mode: 'no-cors',
+  withCredentials: true,
+  credentials: 'same-origin',
 });
 export default instance;
