d270362a037b51398e9e3952b9e04dae79676054
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 23:41:02 -0300

    change origin

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
-  withCredentials: true
-  
-  /* mode: 'no-cors', */
+  mode: 'no-cors',
 });
 export default instance;
