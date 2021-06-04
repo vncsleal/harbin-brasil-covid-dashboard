b58170bcf6e922298d5f208fa21b628901904bf4
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 23:17:20 -0300

    test without with credentials

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ const instance = axios.create({
   baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
   headers: {
     'Access-Control-Allow-Origin' : 'https://api.brasil.io',
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Content-Type': 'application/x-www-form-urlencoded'
   }
 });
 export default instance;
