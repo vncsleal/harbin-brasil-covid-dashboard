45fbe8c3be5a2e8b5058a310c2912b9b74c89f54
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 23:26:18 -0300

    change origin

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://api.brasil.io',
     'Content-Type': 'application/json',
   },
   mode: 'no-cors',
