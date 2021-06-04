daf9a343266ba5a6fb2d965fb32d23e0fef1cb1f
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 23:37:28 -0300

    change origin

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-  },
-  mode: 'no-cors',
+  baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
+  withCredentials: true
+  
+  /* mode: 'no-cors', */
 });
 export default instance;
