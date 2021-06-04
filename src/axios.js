178bb8696c9f29fd98943ceec37f04ec0def6617
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 22:29:01 -0300

    test base url

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
+  baseURL: 'https://api.brasil.io/v1/dataset/covid19/caso_full/data/',
   headers: {
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    'Access-Control-Allow-Credentials': true
+    'Access-Control-Allow-Credentials': true,
+    'Content-Type': 'application/x-www-form-urlencoded'
   }
 });
 export default instance;
