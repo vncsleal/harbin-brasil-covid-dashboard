e2a7cd3f45057539277dab3ceee1ab9e23b212eb
vitorvicentecorreia <vitorvicentecorreia@gmail.com>
2021-06-03 22:40:14 -0300

    test proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+    app.use(
+        proxy("/v1/dataset/covid19/caso_full/data", {
+            target: "https://api.brasil.io",
+            changeOrigin: true
+        })
+    )
+}
